5d6b0b616f02909331fc0e8dc4bbc97b114940d9
Yuliia Shevchenko <133769004+j-sheva@users.noreply.github.com>
2024-05-20 18:27:22 +0200

    hw 34

--- 34_homework/min.js
+++ 34_homework/min.js
@@ -1,96 +1,101 @@
-const shoppingList = [
-  {
-    name: 'Milk',
-    quantity: 2,
-    purchased: false,
-    pricePerUnit: 1.5,
-    total: 3.0,
-  },
-  {
-    name: 'Bread',
-    quantity: 1,
-    purchased: true,
-    pricePerUnit: 1.0,
-    total: 1.0,
-  },
-  {
-    name: 'Eggs',
-    quantity: 12,
-    purchased: false,
-    pricePerUnit: 0.1,
-    total: 1.2,
-  },
-  {
-    name: 'Cheese',
-    quantity: 1,
-    purchased: true,
-    pricePerUnit: 2.5,
-    total: 2.5,
-  },
-  {
-    name: 'Butter',
-    quantity: 1,
-    purchased: false,
-    pricePerUnit: 1.8,
-    total: 1.8,
-  },
-];
+const shoppingList = [];
 
-function displayShoppingList(list) {
-  console.log('Shopping List:');
-
-  console.log('Not Purchased:');
-  list.forEach((item) => {
-    if (!item.purchased) {
-      console.log(
-        `- ${item.name}: ${item.quantity} Price: ${item.pricePerUnit} Total price: ${item.total}`
-      );
-    }
-  });
-
-  console.log('Purchased:');
-  list.forEach((item) => {
-    if (item.purchased) {
-      console.log(
-        `- ${item.name}: ${item.quantity} Price: ${item.pricePerUnit} Total price: ${item.total}`
-      );
-    }
-  });
+function Product(name, quantity, purchased, pricePerUnit) {
+  (this.name = name),
+    (this.quantity = quantity),
+    (this.purchased = purchased),
+    (this.pricePerUnit = pricePerUnit),
+    (this.total = this.quantity * this.pricePerUnit);
 }
 
+let product1 = new Product('Milk', 2, false, 1.5);
+let product2 = new Product('Bread', 1, true, 1.0);
+let product3 = new Product('Eggs', 10, false, 0.1);
+let product4 = new Product('Cheese', 1, true, 1.8);
+let product5 = new Product('Butter', 2, false, 2.5);
+
+shoppingList.push(product1, product2, product3, product4, product5);
+
+function displayShoppingList(list) {
+  list.sort((a, b) => a.purchased - b.purchased);
+  console.log(shoppingList);
+}
 displayShoppingList(shoppingList);
+
+// Функція для відзначення продукту як придбаний
+buyProduct = function (productName) {
+  const product = shoppingList.find((product) => product.name === productName);
+  if (product) {
+    product.purchased = true;
+    console.log(` "${productName}" is purchased.`);
+  } else {
+    console.log(` "${productName}" not found.`);
+  }
+};
+buyProduct('Milk');
 
 // Видалення продукту
 
-function removeProduct() {
-  let { shoppingList, ...rest } = shoppingList;
-  return rest;
-}
+removeProduct = function (productName) {
+  const newList = shoppingList.filter(
+    (product) => product.name !== productName
+  );
+  console.log(` "${productName}" is deleted.`);
+  console.log(newList);
+  return newList;
+};
 
-const updatedListAfterRemoval = removeProduct(shoppingList, 'Bread');
-console.log('After removal of Bread:');
-displayShoppingList(updatedListAfterRemoval);
+removeProduct('Bread');
 
-// Додавання або оновлення продукту
-
-function addOrUpdateProduct(list, product) {
-  const existingProduct = list.find((item) => item.name === product.name);
+// Функція для додавання покупки в список
+addProduct = function (name, quantity) {
+  const existingProduct = shoppingList.find((product) => product.name === name);
   if (existingProduct) {
-    existingProduct.quantity += product.quantity;
+    existingProduct.quantity += quantity;
     existingProduct.total =
       existingProduct.quantity * existingProduct.pricePerUnit;
+    console.log(`The quantity of the product "${name}" has been increased.`);
   } else {
-    list.push(product);
+    const newProduct = new Product(name, quantity);
+    shoppingList.push(newProduct);
+    console.log(`"${name}" has been added `);
   }
-  return list;
-}
+  console.log(shoppingList);
+};
 
-const updatedListAfterAddition = addOrUpdateProduct(shoppingList, {
-  name: 'Milk',
-  quantity: 1,
-  pricePerUnit: 1.5,
-  total: 1.5,
-  purchased: false,
-});
-console.log('After adding/updating Milk:');
-displayShoppingList(updatedListAfterAddition);
+addProduct('Cheese', 1);
+addProduct('Butter', 55);
+
+// Функція для підрахунку суми всіх продуктів
+calculateTotalSum = function () {
+  const totalSum = shoppingList.reduce(
+    (total, product) => total + parseInt(product.total),
+    0
+  );
+  console.log(`Total sum: ${totalSum}$`);
+  return totalSum;
+};
+calculateTotalSum();
+
+// Функція для підрахунку суми всіх придбаних або не придбаних продуктів
+calculateSumByStatus = function (purchased) {
+  const sum = shoppingList
+    .filter((product) => product.purchased === purchased)
+    .reduce((total, product) => total + product.total, 0);
+
+  console.log(`Total sum putchased items: ${sum}$`);
+  return sum;
+};
+calculateSumByStatus(true);
+
+// Функція для сортування продуктів за сумою
+this.sortBySum = function (ascending = true) {
+  shoppingList.sort((a, b) =>
+    ascending ? a.total - b.total : b.total - a.total
+  );
+  console.log(`(${ascending ? 'lower --> higher' : 'higher --> lower'}).`);
+};
+sortBySum(true);
+displayShoppingList(shoppingList);
+sortBySum(false);
+displayShoppingList(shoppingList);
