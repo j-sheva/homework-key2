2732139cbfcc4ee6e8e66e252aca438781d4726f
Yuliia Shevchenko <133769004+j-sheva@users.noreply.github.com>
2024-06-26 13:10:28 +0200

    HW 43 fixed

--- 43_homework/clocks.js
+++ 43_homework/clocks.js
@@ -1,79 +1,75 @@
+const form = document.getElementById('time-form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const inputTimezone = e.target.querySelector('input#timezone')?.value;
+
+  if (inputTimezone) {
+    new WorldClock(inputTimezone);
+  } else {
+    alert('Enter time zone please');
+  }
+});
+
 class WorldClock {
   constructor(timezone) {
     this.timezone = timezone;
     this.clockElement = document.createElement('div');
     this.clockElement.classList.add('clock');
     this.render();
+    this.addClickListener();
+  }
+
+  addClickListener() {
+    this.clockElement.addEventListener('click', (e) => {
+      const { target } = e;
+      if (target.closest('.button1')) {
+        this.getCurrentDate();
+      } else if (target.closest('.button2')) {
+        this.getCurrentDateTime();
+      } else if (target.closest('.button3')) {
+        this.deleteClock();
+      }
+    });
   }
 
   getCurrentDate() {
-    const now = new Date();
-    return now.toLocaleDateString('uk-UA', { timeZone: this.timezone });
+    try {
+      const date = new Date().toLocaleDateString('uk-UA', {
+        timeZone: this.timezone,
+      });
+
+      this.clockElement.querySelector('.time-output').textContent = date;
+    } catch (err) {
+      alert('Incorrect time zone');
+    }
   }
 
   getCurrentDateTime() {
-    const now = new Date();
-    return now.toLocaleString('uk-UA', { timeZone: this.timezone });
+    try {
+      const time = new Date().toLocaleString('uk-UA', {
+        timeZone: this.timezone,
+      });
+
+      this.clockElement.querySelector('.time-output').textContent = time;
+    } catch (err) {
+      alert('Incorrect time zone');
+    }
+  }
+
+  deleteClock() {
+    this.clockElement.remove();
   }
 
   render() {
     this.clockElement.innerHTML = `
     <p class="title">Time zone: ${this.timezone}</p>
    
-    <button class="button button1" onclick="showDate(this)">Current date</button>
-    <button class="button button2" onclick="showDateTime(this)">Current date&time</button>
-    <button class="button button3" onclick="deleteClock(this)">Delete watch</button>
+    <button class="button button1">Current date</button>
+    <button class="button button2">Current date&time</button>
+    <button class="button button3">Delete watch</button>
   
     <div class="time-output"></div>
   `;
     document.getElementById('clockContainer').appendChild(this.clockElement);
   }
-
-  deleteClock() {
-    this.clockElement.remove();
-  }
-}
-
-function addClock() {
-  const timezone = document.getElementById('timezone').value;
-  if (timezone) {
-    new WorldClock(timezone);
-  } else {
-    alert('Enter time zone please');
-  }
-}
-
-function showDate(button) {
-  const clockElement = button.parentElement;
-  const timezone = clockElement.querySelector('p').textContent.split(': ')[1];
-  try {
-    const output = new Date().toLocaleDateString('uk-UA', {
-      timeZone: timezone,
-    });
-    clockElement.querySelector(
-      '.time-output'
-    ).textContent = `Current date: ${output}`;
-  } catch (error) {
-    alert('Incorrect time zone');
-  }
-}
-
-function showDateTime(button) {
-  const clockElement = button.parentElement;
-  const timezone = clockElement.querySelector('p').textContent.split(': ')[1];
-  try {
-    const output = new Date().toLocaleString('uk-UA', {
-      timeZone: timezone,
-    });
-    clockElement.querySelector(
-      '.time-output'
-    ).textContent = `Current date and time: ${output}`;
-  } catch (error) {
-    alert('Incorrect time zone');
-  }
-}
-
-function deleteClock(button) {
-  const clockElement = button.parentElement;
-  clockElement.remove();
 }
